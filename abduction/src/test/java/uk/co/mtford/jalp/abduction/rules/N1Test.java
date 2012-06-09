e1e18ad2ec6a9cdd092ab118abd02928dd061746
Michael Ford <mtf09@imperial.ac.uk>
2012-06-09 19:46:16 +0100

    Added N1 unit test.

--- abduction/src/test/java/uk/co/mtford/jalp/abduction/rules/N1Test.java
+++ abduction/src/test/java/uk/co/mtford/jalp/abduction/rules/N1Test.java
@@ -2,6 +2,13 @@ package uk.co.mtford.jalp.abduction.rules;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import uk.co.mtford.jalp.JALP;
+import uk.co.mtford.jalp.abduction.logic.instance.IInferableInstance;
+import uk.co.mtford.jalp.abduction.parse.query.JALPQueryParser;
+import uk.co.mtford.jalp.abduction.parse.query.ParseException;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,5 +30,14 @@ public class N1Test {
     @After
     public void noTearDown() {
 
+    }
+
+    @Test
+    public void test1() throws Exception {
+        N1RuleNode ruleNode = new N1RuleNode();
+        List<IInferableInstance> goals = JALPQueryParser.readFromString("not p(X,Y,Z)");
+        ruleNode.setGoals(goals);
+        JALP.applyRule(ruleNode);
+        JALP.getVisualizer("debug/rules/N1/Test1",ruleNode);
     }
 }
