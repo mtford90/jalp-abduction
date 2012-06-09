2d7c6931fdc915efc52ff25c7e17a37d1059e847
Michael Ford <mtf09@imperial.ac.uk>
2012-06-09 20:13:56 +0100

    Added E2 unit tests. They break hard because of the query parser atm...

--- abduction/src/test/java/uk/co/mtford/jalp/abduction/rules/E2Test.java
+++ abduction/src/test/java/uk/co/mtford/jalp/abduction/rules/E2Test.java
@@ -2,6 +2,13 @@ package uk.co.mtford.jalp.abduction.rules;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import uk.co.mtford.jalp.JALP;
+import uk.co.mtford.jalp.abduction.logic.instance.DenialInstance;
+import uk.co.mtford.jalp.abduction.logic.instance.IInferableInstance;
+import uk.co.mtford.jalp.abduction.parse.query.JALPQueryParser;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,5 +30,70 @@ public class E2Test {
     @After
     public void noTearDown() {
 
+    }
+
+    @Test
+    public void test1() throws Exception {
+        E2RuleNode ruleNode = new E2RuleNode();
+        List<IInferableInstance> goals = JALPQueryParser.readFromString("X=c, p(X)");
+
+        DenialInstance d = new DenialInstance(goals);
+        d.getUniversalVariables().addAll(goals.get(0).getVariables());
+        ruleNode.getGoals().add(d);
+
+        JALP.applyRule(ruleNode);
+        JALP.getVisualizer("debug/rules/E2/Test1",ruleNode);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        E2RuleNode ruleNode = new E2RuleNode();
+        List<IInferableInstance> goals = JALPQueryParser.readFromString("c=X, p(X)");
+
+        DenialInstance d = new DenialInstance(goals);
+        d.getUniversalVariables().addAll(goals.get(0).getVariables());
+        ruleNode.getGoals().add(d);
+
+        JALP.applyRule(ruleNode);
+        JALP.getVisualizer("debug/rules/E2/Test2",ruleNode);
+    }
+
+    @Test
+    public void test3() throws Exception {
+        E2RuleNode ruleNode = new E2RuleNode();
+        List<IInferableInstance> goals = JALPQueryParser.readFromString("p(X)=q(Y), e(D)");
+
+        DenialInstance d = new DenialInstance(goals);
+        d.getUniversalVariables().addAll(goals.get(0).getVariables());
+        ruleNode.getGoals().add(d);
+
+        JALP.applyRule(ruleNode);
+        JALP.getVisualizer("debug/rules/E2/Test3",ruleNode);
+    }
+
+    @Test
+    public void test4() throws Exception {
+        E2RuleNode ruleNode = new E2RuleNode();
+        List<IInferableInstance> goals = JALPQueryParser.readFromString("X=p(Y), q(X)");
+
+        DenialInstance d = new DenialInstance(goals);
+        d.getUniversalVariables().addAll(goals.get(0).getVariables());
+        ruleNode.getGoals().add(d);
+
+        JALP.applyRule(ruleNode);
+        JALP.getVisualizer("debug/rules/E2/Test1",ruleNode);
+    }
+
+    @Test
+    public void test5() throws Exception {
+        E2RuleNode ruleNode = new E2RuleNode();
+        List<IInferableInstance> goals = JALPQueryParser.readFromString("p(Y)=X, q(X)");
+
+        DenialInstance d = new DenialInstance(goals);
+        d.getUniversalVariables().addAll(goals.get(0).getVariables());
+        ruleNode.getGoals().add(d);
+
+        JALP.applyRule(ruleNode);
+        JALP.getVisualizer("debug/rules/E2/Test1",ruleNode);
     }
 }
